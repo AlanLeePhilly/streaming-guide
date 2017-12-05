423551c1d8bf19ba25ef85bde54c39e98c28f3e7
chris <cefroschle@gmail.com>
2017-12-05 15:34:09 -0500

    added devise

--- Gemfile
+++ Gemfile
@@ -1,13 +1,16 @@
 source 'https://rubygems.org/'
 
-gem 'rails', '~> 5.1.4'
+ruby '2.3.3'
+
+gem 'rails'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
+gem 'devise'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 group :development do
-  gem 'listen', '~> 3.0.5'
+  gem 'listen'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
@@ -21,6 +24,8 @@ group :development, :test do
   gem 'shoulda'
   gem 'valid_attribute'
   gem 'shoulda-matchers', require: false
+
+  gem 'quiet_assets'
 end
 
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,14 @@
 require_relative 'boot'
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -19,7 +18,7 @@ Bundler.require(*Rails.groups)
 module StreamGuide
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
