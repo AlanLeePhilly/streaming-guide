bc47ff355c8bc0ee4545909ec72000b1c7f55a7f
James <jlee71884@gmail.com>
2017-12-07 10:56:02 -0500

    test controllers

---
+++ spec/controllers/programs_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe ProgramsController, type: :controller do
+  describe "GET index" do
+    it "should render index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+  describe "GET show" do
+    it "should render show template" do
+      get :show, params: { id: 1 }
+      expect(response).to render_template("show")
+    end
+  end
+end
