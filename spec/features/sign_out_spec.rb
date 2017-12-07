77a3a15a78d07b3b71b7f2ef184a84cc935f1355
chris <cefroschle@gmail.com>
2017-12-07 17:02:51 -0500

    added user show pages and fixed sign in

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -6,8 +6,8 @@ feature 'sign out' ,%{
   So that I can end my current session from cache
  } do
   # scenario "account holder signs out" do
-  #   click_button 'Sign Out'
+  #   click_link 'Sign Out'
   #
-  #   expect(page).to have_content("Succesfully Signed Out")
+  #   expect(page).to have_content("Signed out successfully")
   # end
 end
