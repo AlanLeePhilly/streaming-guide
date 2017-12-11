49652e0ac6fe0c1d6619f4eaf63c05fc584511ce
chris <cefroschle@gmail.com>
2017-12-11 10:06:19 -0500

    fixed errors

--- spec/features/user_edit_spec.rb
+++ spec/features/user_edit_spec.rb
@@ -1,14 +1,8 @@
 # These are tests for future features for editing your user page
 require 'rails_helper'
 
-# feature 'edit profile', %(
-#    As a user
-#    I want to edit my profile
-#    So that I can stand out on this fancy site
-# ) do
 feature 'edit profile' do
-  let!(:user) {FactoryBot.create(:user)}
-
+  let!(:user) { FactoryBot.create(:user) }
 
   scenario 'vistor tries to navigate to an edit page' do
     visit '/users/edit'
@@ -52,52 +46,3 @@ feature 'edit profile' do
     expect(page).to have_content 'Your account has been updated successfully.'
   end
 end
-
-
-
-
-
-
-  #
-  # scenario 'user fills out form with valid values and submits' do
-  #   visit edit_user_registration_path
-  #   id = user.id
-  #   fill_in 'Username', with: 'cbogbog'
-  #   fill_in 'Email', with: 'cbog@gmail.com'
-  #   fill_in 'Password', with: 'passwordtest'
-  #   fill_in 'Confirm Password', with: 'passwordtest'
-  #   fill_in 'Current Password', with: user.password
-  #
-  #   click_on 'Update'
-  #
-  #   user = User.find(id)
-  #   expect(page).to have_content 'Your account has been updated successfully.'
-  #   expect(user.username).to eq 'cbogbog'
-  #   expect(user.email).to eq 'cbog@gmail.com'
-  # end
-  # scenario 'confirmation password does not match new password' do
-  #   visit edit_user_registration_path
-  #   id = user.id
-  #   fill_in 'Username', with: 'cbogbog'
-  #   fill_in 'Email', with: 'cbog@gmail.com'
-  #   fill_in 'Current Password', with: user.password
-  #   fill_in 'New Password', with: 'passwordtest'
-  #   fill_in 'Confirm Password', with: 'doesnotmatch'
-  #
-  #   click_on 'Update'
-  #
-  #   expect(page).to have_content 'Password confirmation doesn\'t match Password'
-  # end
-  #
-  #
-  # scenario 'user does not fill out current password' do
-  # visit edit_user_registration_path
-  # id = user.id
-  # fill_in 'Username', with: 'cbogbog'
-  # fill_in 'Email', with: 'cbog@gmail.com'
-  # fill_in 'Password', with: 'passwordtest'
-  # fill_in 'Confirm Password', with: 'passwordtest'
-  #
-  # click_on 'Update'
-  #
-  # expect(page).to have_content 'Current password can\'t be blank'
